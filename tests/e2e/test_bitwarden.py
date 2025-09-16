1b57bd719ebb7018430cc537853ee5111811c5c7
Kyattsukuro <nikl.free44@gmail.com>
2025-09-16 11:24:04 +0200

    fix: unittests by adding test-account-3@example.com

--- tests/e2e/test_bitwarden.py
+++ tests/e2e/test_bitwarden.py
@@ -41,7 +41,7 @@ class BitwardenBasic(unittest.TestCase):
         ).users()
 
     def test_get_organization_users(self):
-        self.assertEqual(len(self.test_users), 2)
+        self.assertEqual(len(self.test_users), 3)
 
     def test_get_organization_items(self):
         self.assertEqual(len(self.test_org_ciphers), 1)
@@ -63,8 +63,8 @@ class BitwardenBasic(unittest.TestCase):
     def test_get_users_of_collection_2(self):
         self.assertEqual(len(self.test_collection_2_users), 1)
 
-    def test_chang_user_type_organization(self):
-        user = self.organization.user_search("test-account-2@example.com")
+    def test_change_user_type_organization(self):
+        user = self.organization.user_search("test-account-3@example.com")
         resp = self.organization.change_user_type(user, OrganizationUserType.Admin)
         self.assertTrue(resp.is_success)
         resp = self.organization.change_user_type(user, OrganizationUserType.User)
